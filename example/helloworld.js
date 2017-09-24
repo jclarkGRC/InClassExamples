f065a2fa23e90c76ad3adcf0aa7e59b35a9c7846
vdublunatic <jclark4@mail.greenriver.edu>
2017-09-24 16:58:14 -0700

    added button counter

--- example/helloworld.js
+++ example/helloworld.js
@@ -2,5 +2,19 @@
     This is a basic script
  */
 
+var x = 10;
+
+var counter = 1;
+
+function countUp () {
+    var newCount = counter++;
+    document.getElementById("clicks").innerHTML = newCount;
+}
+
+function countUpFrom10 () {
+    var newCount = x++;
+
+    document.getElementById("clicks").innerHTML = newCount;
+}
 
 document.write("I love javascript.");
