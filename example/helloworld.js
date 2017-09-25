366b862a56a19fd44562f9e243ff4e86b5152916
vdublunatic <jclark4@mail.greenriver.edu>
2017-09-25 16:26:37 -0700

    added comments to helloworld.js and added powerpoint

--- example/helloworld.js
+++ example/helloworld.js
@@ -2,11 +2,15 @@
     This is a basic script
  */
 
+//fields or variables
 var counter = 1;
+var newCount;
 
-function countUp () {
-    var newCount = counter++;
+//method or function
+function countUp() {
+    newCount = counter++;
     document.getElementById("clicks").innerHTML = newCount;
 }
 
+//lets send some text to the html page
 document.write("I love javascript. It's the best");
